b87782910b65f2ac0a58ad3d7c418e6d5109f03a
김원식 <kws1044@gmail.com>
2024-06-30 11:32:14 +0900

    영상 1 - GradeAdvanceService TARGET_GEN_FAILED

--- src/test/java/com/example/demo/GradeAdvanceServiceTest.java
+++ src/test/java/com/example/demo/GradeAdvanceServiceTest.java
@@ -1,38 +1,80 @@
 package com.example.demo;
 
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class GradeAdvanceServiceTest {
 
+    private final States state = new States(Paths.get("target/state"));
+    TargetGen mockGen = mock(TargetGen.class);
+    GradeAdvanceService service = new GradeAdvanceService(state, mockGen);
+
+    @BeforeEach
+    void setUp() throws IOException {
+        Files.deleteIfExists(Paths.get("target/state"));
+    }
+
+
     @Test
     void alreadyCompleted(){
-        States state = new States(Paths.get("target/state"));
         state.set(AdvanceState.COMPLETED);
-
-        GradeAdvanceService service = new GradeAdvanceService(state);
-        Assertions.assertThatCode(
-                () -> service.advance()
-        ).isInstanceOf(AlreadyCompletedException.class);
+        AdvanceResult result = service.advance();
+        assertThat(result).isEqualTo(AdvanceResult.ALREADY_COMPLETED);
     }
 
-    private class AlreadyCompletedException extends RuntimeException{
+
+    @Test
+    void targetGenFail(){
+        BDDMockito.given(mockGen.gen()).willThrow(new RuntimeException("!"));
+
+        AdvanceResult result = service.advance();
+        assertThat(result).isEqualTo(AdvanceResult.TARGET_GET_FAILED);
     }
+
 
     private class GradeAdvanceService {
         private final States states;
+        private final TargetGen targetGen;
 
-        public GradeAdvanceService(States state) {
+        public GradeAdvanceService(States state, TargetGen targetGen) {
             this.states = state;
+            this.targetGen = targetGen;
         }
 
-        public void advance() {
+        public AdvanceResult advance() {
             AdvanceState state = states.get();
             if(state == AdvanceState.COMPLETED){
-                throw new AlreadyCompletedException();
+                return AdvanceResult.ALREADY_COMPLETED;
             }
+
+            try {
+                targetGen.gen();
+            } catch (Exception e) {
+                return AdvanceResult.TARGET_GET_FAILED;
+            }
+
+            return null;
+        };
+    }
+
+    private class TargetGen {
+        public Targets gen() {
+            return null;
         }
+    }
+
+    private class Targets {
+    }
+
+    private enum AdvanceResult {
+        TARGET_GET_FAILED, ALREADY_COMPLETED
     }
 }
