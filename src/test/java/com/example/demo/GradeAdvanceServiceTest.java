1eddba2b312f218dbc54708ce0cfffdb98506f63
김원식 <kws1044@gmail.com>
2024-06-30 11:51:51 +0900

    영상1 - GradeAdvanceService TargetsExporters 추가

--- src/test/java/com/example/demo/GradeAdvanceServiceTest.java
+++ src/test/java/com/example/demo/GradeAdvanceServiceTest.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
+import org.mockito.Mockito;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,7 +17,9 @@ public class GradeAdvanceServiceTest {
 
     private final States state = new States(Paths.get("target/state"));
     TargetGen mockGen = mock(TargetGen.class);
-    GradeAdvanceService service = new GradeAdvanceService(state, mockGen);
+    private TargetsExporter mockExporter = mock(TargetsExporter.class);
+
+    GradeAdvanceService service = new GradeAdvanceService(state, mockGen, mockExporter);
 
     @BeforeEach
     void setUp() throws IOException {
@@ -39,14 +43,26 @@ public class GradeAdvanceServiceTest {
         assertThat(result).isEqualTo(AdvanceResult.TARGET_GET_FAILED);
     }
 
+    @Test
+    void targetExportFail(){
+        BDDMockito.given(mockGen.gen()).willReturn(mock(Targets.class));
+        BDDMockito.willThrow(new RuntimeException(("!")))
+                .given(mockExporter).export(Mockito.any(Path.class), Mockito.any(Targets.class));
+
+        AdvanceResult result = service.advance();
+        assertThat(result).isEqualTo(AdvanceResult.TARGET_EXPORT_FAILED);
+    }
+
 
     private class GradeAdvanceService {
         private final States states;
         private final TargetGen targetGen;
+        private final TargetsExporter targetsExporter;
 
-        public GradeAdvanceService(States state, TargetGen targetGen) {
+        public GradeAdvanceService(States state, TargetGen targetGen, TargetsExporter targetsExporter) {
             this.states = state;
             this.targetGen = targetGen;
+            this.targetsExporter = targetsExporter;
         }
 
         public AdvanceResult advance() {
@@ -55,10 +71,17 @@ public class GradeAdvanceServiceTest {
                 return AdvanceResult.ALREADY_COMPLETED;
             }
 
+            Targets targets;
             try {
-                targetGen.gen();
+                targets = targetGen.gen();
             } catch (Exception e) {
                 return AdvanceResult.TARGET_GET_FAILED;
+            }
+
+            try {
+                targetsExporter.export(Paths.get("target/targets"),targets);
+            } catch (Exception e) {
+                return AdvanceResult.TARGET_EXPORT_FAILED;
             }
 
             return null;
@@ -75,6 +98,12 @@ public class GradeAdvanceServiceTest {
     }
 
     private enum AdvanceResult {
-        TARGET_GET_FAILED, ALREADY_COMPLETED
+        TARGET_GET_FAILED, TARGET_EXPORT_FAILED, ALREADY_COMPLETED
+    }
+
+    private class TargetsExporter {
+        public void export(Path path, Targets targets) {
+
+        }
     }
 }
